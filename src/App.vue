059ad35a34234c2d09d7792ed7017c051b09ee72
the-way <2216783205@qq.com>
2025-07-11 21:52:51 +0800

    父子组件传参

--- src/App.vue
+++ src/App.vue
@@ -1,38 +1,11 @@
 <script setup>
-import Index from './layout/Index.vue';
-
-
+    import Father from './components/Father.vue';
 </script>
 
 <template>
-  <!-- <div class="xg-test">
-        我是小乖
-    </div>
-    <div class="xg-test__inner">
-        element
-    </div>
-    <button class="xg-test--success">
-        modifier
-    </button> -->
-
-
-    <Index></Index>
+<Father></Father>
 </template>
 
 <style lang="scss">
-// @include b(test) {
-//     color: red;
-//     @include e(inner) {
-//         color: green;
-//     }
-//     @include m(success){
-//         color: blue;
-//     }
-// }
-
-#app {
-  @include bfc();
-}
-
 
 </style>
---
+++ src/components/Son.vue
@@ -0,0 +1,36 @@
+<template>
+  <div>
+    <div>子集</div>
+    {{ title }}
+
+    <button @click="emitToParent">点击向父组件发送值</button>
+  </div>
+</template>
+
+<script setup>
+import { reactive } from 'vue';
+
+// 子组件接收父组件的值 defineProps
+defineProps({
+  title: {
+    type: String,
+    default: "the-way",
+  },
+});
+
+// 子组件向父组件传递值 defineEmits
+const emit = defineEmits(["on-click"]);
+const list = reactive([1,2,3]);
+
+const emitToParent = () => {
+    emit("on-click", list);
+}
+
+
+// 子组件给父组件暴露属性 defineExpose
+defineExpose({
+    list
+})
+</script>
+
+<style lang="scss" scoped></style>
